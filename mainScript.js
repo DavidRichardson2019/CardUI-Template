d259781c1bbd97e79e51875374586b69381a8605
IndyBob2019 <davidrichardson@Davids-MacBook-Pro.local>
2016-10-24 21:09:39 -0400

    Added animation

--- mainScript.js
+++ mainScript.js
@@ -5,19 +5,19 @@ $(document).ready(function(){
         switch (currentSlide){
             case 1:
                 //Hide 1 show 3
-                transition("#card3", "#card1")
+                transition("#card3", "#card1", "back")
                 currentSlide = 3
                 updapteNumber()
                 break
             case 2:
                 //Hide 2 show 1
-                transition("#card1", "#card2")
+                transition("#card1", "#card2", "back")
                 currentSlide = 1
                 updapteNumber()
                 break
             case 3:
                 //Hide 3 show 2
-                transition("#card2", "#card3")
+                transition("#card2", "#card3", "back")
                 currentSlide = 2
                 updapteNumber()
                 break
@@ -28,19 +28,19 @@ $(document).ready(function(){
          switch (currentSlide){
             case 1:
                 //Hide 1 show 2
-                transition("#card2", "#card1")
+                transition("#card2", "#card1", "next")
                 currentSlide = 2
                 updapteNumber()
                 break
             case 2:
                 //Hide 2 show 3
-                transition("#card3", "#card2")
+                transition("#card3", "#card2", "next")
                 currentSlide = 3
                 updapteNumber()
                 break
             case 3:
                 //Hide 3 show 1
-                transition("#card1", "#card3")
+                transition("#card1", "#card3", "next")
                 currentSlide = 1
                 updapteNumber()
                 break       
@@ -50,19 +50,19 @@ $(document).ready(function(){
         switch (currentSlide){
             case 1:
                 //Hide 1 show 3
-                transition("#card3", "#card1")
+                transition("#card3", "#card1", "back")
                 currentSlide = 3
                 updapteNumber()
                 break
             case 2:
                 //Hide 2 show 1
-                transition("#card1", "#card2")
+                transition("#card1", "#card2", "back")
                 currentSlide = 1
                 updapteNumber()
                 break
             case 3:
                 //Hide 3 show 2
-                transition("#card2", "#card3")
+                transition("#card2", "#card3", "back")
                 currentSlide = 2
                 updapteNumber()
                 break
@@ -73,28 +73,42 @@ $(document).ready(function(){
          switch (currentSlide){
             case 1:
                 //Hide 1 show 2
-                transition("#card2", "#card1")
+                transition("#card2", "#card1", "next")
                 currentSlide = 2
                 updapteNumber()
                 break
             case 2:
                 //Hide 2 show 3
-                transition("#card3", "#card2")
+                transition("#card3", "#card2", "next")
                 currentSlide = 3
                 updapteNumber()
                 break
             case 3:
                 //Hide 3 show 1
-                transition("#card1", "#card3")
+                transition("#card1", "#card3", "next")
                 currentSlide = 1
                 updapteNumber()
                 break       
          }
     })
-    function transition(to, from){
-        
-        $(from).hide()
-        $(to).show()
+    function transition(to, from, direction){
+        if(direction == "next"){
+            $(from).animate({left: '200%'}, function(){
+                $(from).hide()
+                $(to).css({left: '-200%'})
+                $(to).show()
+                $(to).animate({left: '0%'})
+                  
+            })
+            
+        }else if(direction == "back"){
+            $(from).animate({left: '-200%'}, function(){
+                $(from).hide()
+                $(to).css({left: '200%'})
+                $(to).show()
+                $(to).animate({left: '0%'})
+            })
+        }
     }
     function updapteNumber(){
         $("#footerCardNumber").text(currentSlide + " / 3")
